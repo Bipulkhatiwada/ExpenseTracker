f295276011ad398b06d1516162620a4abf4b395d
bipul khatiwada <bipul.khatiwada@swifttech.com.np>
2023-08-28 17:28:26 +0545

    app storage not stored for arrays

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -9,28 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var numbers = [Int]()
+    @State private var numbers = UserDefaults.standard.array(forKey: "counterValues") as? [Int] ?? []
+//    @AppStorage("tapCounter") private var numbers = [Int]()
+    @State private var newNumber = 1
      
     var body: some View {
         NavigationView {
-                List {
+            List {
+                Section {
+                    Button("Tap Counter") {
+                        self.numbers.append(newNumber)
+                        UserDefaults.standard.set(numbers, forKey: "counterValues")
+                        newNumber += 1
+                    }
+                }
+                Section {
                     ForEach(numbers, id: \.self) { number in
                         Text("\(number)") // Use 'number' directly here
                     }.onDelete(perform: removeRows)
-                    Button("Add"){
-                        self.numbers.append(1)
-                    }
                 }
-                .toolbar{
-                    EditButton()
-                }
-
+                
+            }
+            .toolbar {
+                EditButton()
+            }
             .navigationBarTitle("Number List")
         }
     }
     
-    func removeRows(at offsets: IndexSet){
+    func removeRows(at offsets: IndexSet) {
         self.numbers.remove(atOffsets: offsets)
+        UserDefaults.standard.set(numbers, forKey: "counterValues")
     }
 }
 
@@ -39,4 +48,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
