399569af0a3ebe08813f23c10ea75504045419bd
bipul khatiwada <bipul.khatiwada@swifttech.com.np>
2023-08-28 12:42:53 +0545

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Bipul Khatiwada on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
